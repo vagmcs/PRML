00df732a801182a1e509e3889d2348529c1799f9
Evangelos Michelioudakis <vagmcs@gmail.com>
2020-11-12 20:12:00 +0200

    feat(chapter1): Gaussian distribution ML notes, proofs and plots
    * Added ml method for computing the maximum likelihood estimator for mean and variance.

--- prml/distribution/gaussian.py
+++ prml/distribution/gaussian.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 from typing import Optional
 from prml.distribution.distribution import Distribution
@@ -11,6 +12,16 @@ class Gaussian(Distribution):
         self.mu = mu
         self.var = var
 
+    def fit_ml(self, x: np.ndarray, unbiased: bool):
+        self.mu = np.mean(x)
+        if unbiased:
+            self.var = np.sum(np.power(x - self.mu, 2)) / (x.size - 1)
+        else:
+            self.var = np.var(x)
+
     def pdf(self, x):
         d = x - self.mu
         return np.exp(-d ** 2 / (2 * self.var)) / np.sqrt(2 * np.pi * self.var)
+
+    def sample(self, sample_size):
+        return np.random.normal(loc=self.mu, scale=math.sqrt(self.var), size=(sample_size,))
