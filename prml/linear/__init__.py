0f4535b07f94d6ee2327010eb288dd40e64954da
Evangelos Michelioudakis <vagmcs@gmail.com>
2021-03-09 18:26:17 +0200

    feat(linear): gradient descent implementation

---
+++ prml/linear/__init__.py
@@ -0,0 +1,7 @@
+from prml.linear.linear_regression import LinearRegression
+from prml.linear.ridge_regression import RidgeRegression
+
+__all__ = [
+    "LinearRegression",
+    "RidgeRegression"
+]
--- prml/linear/linear_regression.py
+++ prml/linear/linear_regression.py
@@ -25,6 +25,27 @@ class LinearRegression(Regression):
         self.w = np.linalg.pinv(x) @ t
         self.var = np.mean(np.square(x @ self.w - t))
 
+    def fit_lms(self, x: np.ndarray, t: np.ndarray, eta: float = 0.01, n_iter: int = 1000):
+        """
+        Stochastic gradient descent using the sum of squares error
+        function is called Least Mean Squares (LMS).
+
+        :param x: (N, D) numpy array holding the input training data
+        :param t: (N,) numpy array holding the target values
+        :param eta: learning rate
+        :param n_iter: number of iterations
+        """
+
+        x = x[:, None] if x.ndim == 1 else x
+        n, d = x.shape
+        indices = np.arange(n)
+
+        self.w = np.random.random(d)
+        for _ in range(n_iter):
+            indices = np.random.permutation(indices)  # shuffle the data
+            for i in indices:
+                self.w = self.w + eta * (t[i] - np.dot(self.w.T, x[i])) * x[i]
+
     def predict(self, x: np.ndarray, return_std: bool = False):
         """
         Makes a prediction given an input.
@@ -32,12 +53,12 @@ class LinearRegression(Regression):
         :param x: (N, D) numpy array sample to predict their output
         :param return_std: bool, optional returns standard deviation of each prediction if True
         :return:
-        (N,) numpy array holding the prediction of each input
+        (N,) numpy array holding the prediction of each input, and
         (N,) numpy array holding the standard deviation of each prediction
         """
 
         y = x @ self.w
-        if return_std:
+        if return_std and self.var is not None:
             y_std = np.sqrt(self.var) + np.zeros_like(y)
             return y, y_std
         return y
