d02bf3450c7a7cd30e4f97627842c4de5fc61abe
Evangelos Michelioudakis <vagmcs@gmail.com>
2023-08-16 14:02:02 +0300

    feat(preprocessing): adds k arg to one-hot encoder
    - Fixes minor math typo in chapter 4 notebook.

--- prml/preprocessing/one_hot_encoder.py
+++ prml/preprocessing/one_hot_encoder.py
@@ -4,25 +4,33 @@ import numpy as np
 
 class OneHotEncoder(object):
     """
-    One-encoder encoder.
+    OneHotEncoder encodes arrays of classes into 1-of-K class vectors.
     """
 
-    @staticmethod
-    def encode(class_indices: np.ndarray) -> np.ndarray:
+    def __init__(self, k: int = None) -> None:
         """
-        Encodes an array of class indices into one-of-k coding.
+        Creates an encoder given a number of classes. In case the number classes is not provided,
+        they are automatically inferred during the encoding step from the given array.
+
+        :param k: the number of classes, defaults to None
+        """
+        self._k = k
+
+    def encode(self, class_indices: np.ndarray) -> np.ndarray:
+        """
+        Encodes an array of class indices into one-of-K coding.
 
         :param class_indices: (N,) array of non-negative class indices
         :return: (N, K) array holding the one-of-K encodings
         """
 
-        n_classes = np.max(class_indices) + 1
+        n_classes = np.max(class_indices) + 1 if self._k is None else self._k
         return np.eye(n_classes)[class_indices]  # type: ignore
 
     @staticmethod
     def decode(onehot: np.ndarray) -> np.ndarray:
         """
-        Decodes the one-of-k code into class indices
+        Decodes the one-of-K code into class indices.
 
         :param onehot: (N, K) array containing one-of-K codings
         :return: (N,) array of class indices
