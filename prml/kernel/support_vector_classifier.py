5ec195469fb925b831d47ee8ce1229d1653e86d5
Evangelos Michelioudakis <vagmcs@gmail.com>
2024-02-16 12:20:39 +0100

    feat(ch7): adds svm examples

--- prml/kernel/support_vector_classifier.py
+++ prml/kernel/support_vector_classifier.py
@@ -8,116 +8,134 @@ from prml.linear.classifier import Classifier
 
 class SupportVectorClassifier(Classifier):
     """
-    Support Vector Machine model.
-    Sequential Minimal Optimization.
-    https://www.microsoft.com/en-us/research/wp-content/uploads/2016/02/tr-98-14.pdf
+    Support Vector Machine classifier.
     """
 
-    def __init__(self, kernel: Kernel, C: float = 1, max_iter=100) -> None:
+    def __init__(self, kernel: Kernel, C: float = 1) -> None:
+        """
+        Creates a support vector classifier.
+        """
         super().__init__()
         self.kernel = kernel
-        self.alpha = None
-        self.b = None
-        self.support_labels = None
-        self.support_vectors = None
         self.c = C
-        self.max_iter = max_iter
-        self.kkt_thr = 1e-8
-
-        # self.b = np.array([])  # SVM's threshold
-        # self.alpha = np.array([])  # Alpha parameters of the support vectors
-        # self.support_vectors = np.array([])  # Matrix in which each row is a support vector
-        # self.support_labels = np.array([])  # Vector with the ground truth labels of the support vectors
-
-    def predict(self, x: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
-        if x.ndim == 1:
-            x = x[None, :]
-
-        k = self.kernel(self.support_vectors, x) if self.kernel else self.support_vectors @ x.T
-        # print(self.support_vectors.shape, x.shape, k.shape, (self.alpha * self.support_labels).shape)
-        f = (self.alpha * self.support_labels) @ k + self.b
-        return np.where(f >= 0, 1, -1), f
-
-    def fit(self, x_train: np.ndarray, y_train: np.ndarray) -> None:
-        N, D = x_train.shape
+        self.alpha = None
         self.b = 0
-        self.alpha = np.zeros(N)
-        self.support_labels = y_train
-        self.support_vectors = x_train
+        self._support_vectors = None
+        self._support_labels = None
 
-        gram = self.kernel(x_train, x_train) if self.kernel else x_train @ x_train.T
-        non_kkt_array = np.arange(N)  # Iterate all indices first
-        error_cache = self.predict(x_train)[1] - y_train  # auxilary array for heuristics
+    @property
+    def n_support_vectors(self) -> int:
+        return 0 if self._support_vectors is None else len(self._support_vectors)
+
+    @property
+    def support_vectors(self) -> np.ndarray:
+        return np.empty(0) if self._support_vectors is None else self._support_vectors
+
+    def fit(self, x: np.ndarray, t: np.ndarray, n_iter: int = 100, epsilon: float = 1e-8) -> None:
+        """
+        Performs sequential minimal optimization (SMO).
+
+        https://www.microsoft.com/en-us/research/wp-content/uploads/2016/02/tr-98-14.pdf
+
+        :param x: (N, D) array holding the input training data
+        :param t: (N,) array holding the target values
+        :param n_iter: number of iterations
+        :param epsilon: tolerance threshold for KKT conditions
+        """
+
+        n_examples, _ = x.shape
+        self.alpha = np.zeros(n_examples)
+        self.b = 0
+
+        # initially all data are assumed to be support vectors
+        self._support_vectors = x
+        self._support_labels = t
+
+        # compute Gram matrix and error cache
+        gram = self.kernel(x, x) if self.kernel else x @ x.T
+        error_cache = self.predict(x)[1] - t
+
+        kkt_indices = np.arange(n_examples)
 
         passes = 0
-        while passes < self.max_iter:
-            i_1, non_kkt_array = self.__first_alpha_heuristic(non_kkt_array, self.kkt_thr)
-            if i_1 is None:
+        while passes < n_iter:
+            i, kkt_indices = self.__first_alpha_heuristic(kkt_indices, epsilon)
+            if i is None:
                 break
 
-            i_2 = self.__second_choice_heuristic(error_cache, i_1)
-            if i_1 == i_2:
+            j = self.__second_choice_heuristic(error_cache, i)
+            if i == j:
                 continue
 
-            # Update boundaries
-            L, H = self.__bounds(i_1, i_2)
+            # update boundaries
+            L, H = self.__bounds(i, j)
             if L == H:
                 continue
 
-            # Compute eta
-            eta = gram[i_1][i_1] + gram[i_2][i_2] - 2 * gram[i_1][i_2]
+            # compute eta
+            eta = gram[i][i] + gram[j][j] - 2 * gram[i][j]
             if eta == 0:
                 continue
 
-            # Calculate predictions and errors for x_1 and x_2
-            E_1 = self.predict(self.support_vectors[i_1, :])[1] - self.support_labels[i_1]
-            E_2 = self.predict(self.support_vectors[i_2, :])[1] - self.support_labels[i_2]
+            # calculate predictions and errors
+            error_i = self.predict(self._support_vectors[i, :])[1] - self._support_labels[i]
+            error_j = self.predict(self._support_vectors[j, :])[1] - self._support_labels[j]
 
-            # Update alpha
-            alpha_1_old = self.alpha[i_1]
-            alpha_2_old = self.alpha[i_2]
-            self.alpha[i_2] = alpha_2_old + self.support_labels[i_2] * (E_1 - E_2) / eta
-            self.alpha[i_2] = np.minimum(self.alpha[i_2], H)
-            self.alpha[i_2] = np.maximum(self.alpha[i_2], L)
-            self.alpha[i_1] = alpha_1_old + self.support_labels[i_1] * self.support_labels[i_2] * (
-                alpha_2_old - self.alpha[i_2]
+            # update alpha
+            alpha_1_old = self.alpha[i]
+            alpha_2_old = self.alpha[j]
+            self.alpha[j] = alpha_2_old + self._support_labels[j] * (error_i - error_j) / eta
+            self.alpha[j] = np.minimum(self.alpha[j], H)
+            self.alpha[j] = np.maximum(self.alpha[j], L)
+            self.alpha[i] = alpha_1_old + self._support_labels[i] * self._support_labels[j] * (
+                alpha_2_old - self.alpha[j]
             )
 
-            # Update beta
-            self.__update_beta(i_1, i_2, gram, E_1, E_2, alpha_1_old, alpha_2_old)
+            # update beta
+            self.__update_beta(i, j, gram, error_i, error_j, alpha_1_old, alpha_2_old)
 
-            # Update error cache
-            error_cache[i_1] = self.predict(self.support_vectors[i_1, :])[1] - self.support_labels[i_1]
-            error_cache[i_2] = self.predict(self.support_vectors[i_2, :])[1] - self.support_labels[i_2]
+            # update error cache
+            error_cache[i] = self.predict(self._support_vectors[i, :])[1] - self._support_labels[i]
+            error_cache[j] = self.predict(self._support_vectors[j, :])[1] - self._support_labels[j]
 
-            # fix the errors in order to avoid getting stuck
-            if 0 < self.alpha[i_1] < self.c:
-                error_cache[i_1] = 0
-            if 0 < self.alpha[i_2] < self.c:
-                error_cache[i_2] = 0
+            # adapt errors in order to avoid getting stuck near zero values
+            if 0 < self.alpha[i] < self.c:
+                error_cache[i] = 0
+            if 0 < self.alpha[j] < self.c:
+                error_cache[j] = 0
 
+            # next iteration
             passes += 1
 
-        # Store only support vectors
+        # store only support vectors
         support_vectors_idx = self.alpha != 0
-        self.support_labels = self.support_labels[support_vectors_idx]
-        self.support_vectors = self.support_vectors[support_vectors_idx, :]
+        self._support_labels = self._support_labels[support_vectors_idx]
+        self._support_vectors = self._support_vectors[support_vectors_idx, :]
         self.alpha = self.alpha[support_vectors_idx]
 
-        print(f"Training summary: {passes} iterations, {self.alpha.shape[0]} supprts vectors")
+    def predict(self, x: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+        """
+        Makes a prediction given an input.
 
-    def __second_choice_heuristic(self, error: np.ndarray, i: int):
-        # find all alpha that are neither zero or C
-        non_bounded_alpha_indices = np.argwhere((0 < self.alpha) & (self.alpha < self.c)).reshape(-1)
+        :param x: (N, D) array of samples to predict their output
+        :return a tuple of (N,) arrays, one holding the target predictions, and one the y values
+        """
 
-        # find the alpha index that maximizes the step size |Ea - Eb|
-        if len(non_bounded_alpha_indices) > 0:
-            if error[i] >= 0:
-                return non_bounded_alpha_indices[np.argmin(error[non_bounded_alpha_indices])]
-            else:
-                return non_bounded_alpha_indices[np.argmax(error[non_bounded_alpha_indices])]
-        else:
-            return np.argmax(np.abs(error - error[i]))
+        if self.alpha is None or self._support_vectors is None or self._support_labels is None:
+            raise ValueError("The model is not trained, thus predictions cannot be made!")
+
+        if x.ndim == 1:
+            x = x[None, :]
+
+        k = self.kernel(self._support_vectors, x) if self.kernel else self._support_vectors @ x.T
+        f = (self.alpha * self._support_labels) @ k + self.b
+        return np.where(f >= 0, 1, -1), f
+
+    def __kkt(self, i: int, epsilon: float):
+        t_error = self._support_labels[i] * (self.predict(self._support_vectors[i])[1] - self._support_labels[i])
+        cond_a = t_error < -epsilon and self.alpha[i] < self.c
+        cond_b = t_error > epsilon and self.alpha[i] > 0
+        return cond_a or cond_b
 
     def __first_alpha_heuristic(self, indices: np.ndarray, epsilon: float):
         for idx in indices:
@@ -134,14 +152,21 @@ class SupportVectorClassifier(Classifier):
         else:
             return None, indices
 
-    def __kkt(self, i: int, epsilon: float):
-        t_error = self.support_labels[i] * (self.predict(self.support_vectors[i])[1] - self.support_labels[i])
-        cond_a = t_error < -epsilon and self.alpha[i] < self.c
-        cond_b = t_error > epsilon and self.alpha[i] > 0
-        return cond_a or cond_b
+    def __second_choice_heuristic(self, error: np.ndarray, i: int):
+        # find all alpha that are neither zero or C
+        non_bounded_alpha_indices = np.argwhere((0 < self.alpha) & (self.alpha < self.c)).reshape(-1)
+
+        # find the alpha index that maximizes the step size |Ea - Eb|
+        if len(non_bounded_alpha_indices) > 0:
+            if error[i] >= 0:
+                return non_bounded_alpha_indices[np.argmin(error[non_bounded_alpha_indices])]
+            else:
+                return non_bounded_alpha_indices[np.argmax(error[non_bounded_alpha_indices])]
+        else:
+            return np.argmax(np.abs(error - error[i]))
 
     def __bounds(self, i: int, j: int) -> tuple[float, float]:
-        if self.support_labels[i] == self.support_labels[j]:
+        if self._support_labels[i] == self._support_labels[j]:
             return (max(0, self.alpha[i] + self.alpha[j] - self.c), min(self.c, self.alpha[i] + self.alpha[j]))
         else:
             return (
@@ -153,14 +178,14 @@ class SupportVectorClassifier(Classifier):
         b1 = (
             self.b
             - error_i
-            - self.support_labels[i] * (self.alpha[i] - alpha_i_old) * gram[i][i]
-            - self.support_labels[j] * (self.alpha[j] - alpha_j_old) * gram[i][j]
+            - self._support_labels[i] * (self.alpha[i] - alpha_i_old) * gram[i][i]
+            - self._support_labels[j] * (self.alpha[j] - alpha_j_old) * gram[i][j]
         )
         b2 = (
             self.b
             - error_j
-            - self.support_labels[i] * (self.alpha[i] - alpha_i_old) * gram[i][j]
-            - self.support_labels[j] * (self.alpha[j] - alpha_j_old) * gram[j][j]
+            - self._support_labels[i] * (self.alpha[i] - alpha_i_old) * gram[i][j]
+            - self._support_labels[j] * (self.alpha[j] - alpha_j_old) * gram[j][j]
         )
         if 0 < self.alpha[i] < self.c:
             self.b = b1
